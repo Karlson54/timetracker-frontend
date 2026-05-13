30f542d921ac59807c301b5ff774ff9786aad204
“Karlson54” <anruban2001@gmail.com>
2026-05-13 03:11:21 +0300

    feat: add dynamic export to authentication-related pages

--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+export const dynamic = 'force-dynamic'
+
 import { useEffect } from 'react'
 import { useAuthContext } from '@/lib/AuthContext'
 import { useTranslation } from 'react-i18next'
